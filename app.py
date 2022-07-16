a3439f896c1c92f7fb31b28446d1660dd0d1da23
Sebastien Reaport <sebastien.reaport@obf.ateneo.edu>
2022-07-16 18:26:27 +0800

    Further Testing

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///schedules.db'
+db = SQLAlchemy(app)
+
+class classes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    class_name = db.Column(db.string(200), nullable=False)
 
 @app.route('/')
 def index():
