bc0049fbdb6fa8d29197b674fed791ae5a9c659f
Sebastien Reaport <sebastien.reaport@obf.ateneo.edu>
2022-07-19 13:52:10 +0800

    It's pretty much ok na HAHAHA
    Added the function to have multiple users and their records are now saved

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from datetime import datetime
 import pandas as pd
+import pickle
 
 app = Flask(__name__)
 
 #Create Schedule Base dataframe
 days_of_week = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-
 startTime = 700
 endTime = 2130
 timeSlots = list()
 intervalStart = startTime
-
 while intervalStart < endTime:
     if intervalStart%100 == 0:
         intervalEnd = intervalStart + 30
@@ -20,19 +19,19 @@ while intervalStart < endTime:
     newInterval = str(intervalStart)+"-"+str(intervalEnd)
     timeSlots.append(newInterval)
     intervalStart = intervalEnd
-
 scheduleFillers = list()
 for timeslots in timeSlots:
     scheduleFillers.append("")
-
 testdf = pd.DataFrame(timeSlots,columns=["Time"])
 for day in days_of_week:
     testdf[day] = scheduleFillers
-
 timeslot_and_index = dict(zip(timeSlots, testdf.index))
 
 #Create Classes List
 classRecords = list()
+currentUser = ""
+pathToRecords = ".\\records\\users"
+pathToSchedules = ".\\records\\schedules"
 
 #Function that updates table
 def edit_table(record, action):
@@ -60,10 +59,93 @@ def edit_table(record, action):
             for day in days_with_classes:
                 testdf.loc[time][day] = ""
 
+#Function that get users
+def get_users():
+    try:
+        with open(pathToRecords, "rb") as f:
+            listOfUsers = pickle.load(f)
+    except:
+        listOfUsers = list()
+    finally:
+        return listOfUsers
+
+#Function that gets users' schedules
+def get_user_schedule():
+    try:
+        with open(pathToSchedules, "rb") as f:
+            listOfUserSchedules = pickle.load(f)
+    except:
+        listOfUserSchedules = dict()
+    finally:
+        return listOfUserSchedules
+
+#Function that adds schedules to table
+def add_classes_to_table():
+    for record in classRecords:
+        edit_table(record, "Add")
+
+#Function that deletes all classes
+def del_classes_in_table():
+    for record in classRecords:
+        edit_table(record, "Delete")
+
 #App Code
-@app.route('/')
+@app.route('/', methods= ['POST','GET'])
+def login():
+    if request.method == "POST":
+        formResponse = request.form
+        if "newUser" in formResponse:
+            #Check if username already exists
+            listOfUsers = get_users()
+            for users in listOfUsers:
+                if users == formResponse.get("newUser"):
+                    return redirect(url_for('newUser'))
+            #Add new username to database
+            listOfUsers.append(formResponse.get("newUser"))
+            with open(pathToRecords, "wb") as f:
+                    pickle.dump(listOfUsers,f)
+        elif "deleteUser" in formResponse:
+            #Check if username already exists and then delete it
+            listOfUsers = get_users()
+            if formResponse.get("deleteUser") in listOfUsers:
+                userToDelete = formResponse.get("deleteUser")
+                listOfUsers.remove(userToDelete)
+                listOfUserSchedules = get_user_schedule()
+                if userToDelete in listOfUserSchedules.keys():
+                    del listOfUserSchedules[userToDelete]
+                    with open(pathToSchedules, "wb") as f:
+                        pickle.dump(listOfUserSchedules,f)
+            #Update database            
+            with open(pathToRecords, "wb") as f:
+                pickle.dump(listOfUsers,f)
+        return render_template('login.html', listOfUsers = listOfUsers)
+    else:
+        listOfUsers = get_users()
+        del_classes_in_table()
+        classRecords.clear()
+        return render_template('login.html', listOfUsers = listOfUsers)
+
+@app.route('/calendar', methods= ['POST','GET'])
 def index():
-    return render_template('index.html', table = testdf)
+    global currentUser
+    if request.method == "POST":
+        username = request.form.get("username")
+        currentUser = username
+        #Check if user has an existing schedule
+        listOfUserSchedules = get_user_schedule()
+        if username in listOfUserSchedules.keys():
+            for record in listOfUserSchedules[username]:
+                classRecords.append(record)
+            add_classes_to_table()
+    else:
+        add_classes_to_table()
+        #Update Database
+        listOfUserSchedules = get_user_schedule()
+        updateUserInfo = {currentUser: classRecords}
+        listOfUserSchedules.update(updateUserInfo)
+        with open(pathToSchedules, "wb") as f:
+            pickle.dump(listOfUserSchedules,f)
+    return render_template('calendar.html', table = testdf)
 
 @app.route('/newclass', methods= ['POST','GET'])
 def add_new_class():
@@ -74,7 +156,6 @@ def add_new_class():
             if records["class_name"] == formResponse.get("class_name"):
                 return redirect(url_for('index'))
         classRecords.append(formResponse)
-        edit_table(record = formResponse, action = "Add")
         return redirect(url_for('index'))
     else:
         return render_template('newclass.html', table = testdf)
@@ -87,7 +168,6 @@ def update(className):
             if records["class_name"] == className:
                 edit_table(record = records, action = "Delete")
                 classRecords.remove(records)
-                edit_table(record = formResponse, action = "Add")
                 classRecords.append(formResponse)
                 return redirect(url_for('index'))
     else:
